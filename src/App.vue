a175095888a4f505f189a14c42d12f497473ccb4
Jarrett <1206905682@qq.com>
2020-10-09 11:23:33 +0800

    添加下拉菜单组件示例

--- src/App.vue
+++ src/App.vue
@@ -3,70 +3,70 @@
     <h2>Button组件</h2>
     <h3>设置主题</h3>
     <li>
-      <Button>default</Button>
-      <Button theme="button">button</Button>
-      <Button theme="link">link</Button>
-      <Button theme="text">text</Button>
+      <LazyerButton>default</LazyerButton>
+      <LazyerButton theme="button">button</LazyerButton>
+      <LazyerButton theme="link">link</LazyerButton>
+      <LazyerButton theme="text">text</LazyerButton>
     </li>
     <br/>
     <h3>设置大小</h3>
     <li>
-      <Button theme="link" size="big">bigSize</Button>
-      <Button theme="link">default</Button>
-      <Button size="small" theme="link">small</Button>
+      <LazyerButton theme="link" size="big">bigSize</LazyerButton>
+      <LazyerButton theme="link">default</LazyerButton>
+      <LazyerButton size="small" theme="link">small</LazyerButton>
     </li>
     <br/>
     <h3>设置状态</h3>
     <li>
-      <Button level="main">main</Button>
-      <Button>default</Button>
-      <Button level="danger">danger</Button>
+      <LazyerButton level="main">main</LazyerButton>
+      <LazyerButton>default</LazyerButton>
+      <LazyerButton level="danger">danger</LazyerButton>
     </li>
     <br/>
     <h3>波纹效果</h3>
     <li>
-      <Button ripple>default-ripple</Button>
-      <Button theme="link" ripple>link-ripple</Button>
-      <Button theme="text" ripple>text-ripple</Button>
+      <LazyerButton ripple>default-ripple</LazyerButton>
+      <LazyerButton theme="link" ripple>link-ripple</LazyerButton>
+      <LazyerButton theme="text" ripple>text-ripple</LazyerButton>
     </li>
     <br/>
     <h2>Switch组件</h2>
     <h3>设置主题</h3>
     <li>
-      <Switch v-model:value="bool5"/>
-      <Switch theme="trendy" v-model:value="bool6"/>
+      <LazyerSwitch v-model:value="bool5"/>
+      <LazyerSwitch theme="trendy" v-model:value="bool6"/>
     </li>
     <br/>
     <h3>设置禁用</h3>
     <li>
-      <Switch v-model:value="bool1" disabled/>
-      <Switch v-model:value="bool2" disabled/>
+      <LazyerSwitch v-model:value="bool1" disabled/>
+      <LazyerSwitch v-model:value="bool2" disabled/>
     </li>
     <br/>
     <h3>禁用+主题</h3>
     <li>
-      <Switch theme="trendy" v-model:value="bool1" disabled/>
-      <Switch theme="trendy" v-model:value="bool2" disabled/>
+      <LazyerSwitch theme="trendy" v-model:value="bool1" disabled/>
+      <LazyerSwitch theme="trendy" v-model:value="bool2" disabled/>
     </li>
     <br/>
     <h3>设置加载状态</h3>
     <li>
-      <Switch v-model:value="bool1" loading/>
-      <Switch v-model:value="bool2" loading/>
+      <LazyerSwitch v-model:value="bool1" loading/>
+      <LazyerSwitch v-model:value="bool2" loading/>
     </li>
     <br/>
     <h3>加载状态+主题</h3>
     <li>
-      <Switch theme="trendy" v-model:value="bool1" loading/>
-      <Switch theme="trendy" v-model:value="bool2" loading/>
+      <LazyerSwitch theme="trendy" v-model:value="bool1" loading/>
+      <LazyerSwitch theme="trendy" v-model:value="bool2" loading/>
     </li>
     <br/>
     <h2>Dialog组件</h2>
     <li>
-      <Button @click="showDialog">打开</Button>
-      <Button @click="toggle">toggle</Button>
-      <Dialog v-model:visible="x" :closeOnClickOverlay="true"
-              :ok="f1" :cancel="f2">
+      <lazyer-button @click="showDialog">show</lazyer-button>
+      <lazyer-button @click="toggle">toggle</lazyer-button>
+      <lazyer-dialog v-model:visible="x" :closeOnClickOverlay="true"
+                     :ok="f1" :cancel="f2">
         <template v-slot:content>
           <div>hello</div>
           <div>world</div>
@@ -74,27 +74,51 @@
         <template v-slot:title>
           <strong>加粗标题</strong>
         </template>
-      </Dialog>
+      </lazyer-dialog>
+    </li>
+    <br/>
+    <h2>Dropdown组件</h2>
+    <li>
+      <LazyerDropdown>
+        <template v-slot:title>Hover me</template>
+        <template v-slot:content>
+          <LazyerMenuItem key="1" @click="sayHi"><a href="javascript:;">1st menu item</a></LazyerMenuItem>
+          <LazyerMenuItem key="2"><a href="javascript:;">2st menu item</a></LazyerMenuItem>
+          <LazyerMenuItem key="3"><a href="javascript:;">3st menu item</a></LazyerMenuItem>
+          <LazyerMenuItem key="4"><a href="javascript:;">4st menu item</a></LazyerMenuItem>
+        </template>
+      </LazyerDropdown>
     </li>
     <br/>
     <h2>Tabs组件</h2>
     <li>
-      <Tabs v-model:selected="chosen">
-        <Tab title="导航一">内容一</Tab>
-        <Tab title="导航二">内容二</Tab>
-      </Tabs>
+      <LazyerTabs v-model:selected="chosen">
+        <LazyerTabsItem title="导航一">内容一</LazyerTabsItem>
+        <LazyerTabsItem title="导航二">内容二</LazyerTabsItem>
+      </LazyerTabs>
     </li>
+    <br/>
+
   </ol>
 </template>
 
 <script>
-import {Button, Switch, Tab, Tabs, Dialog, openDialog} from 'lazyer-ui-1'
+import {
+  LazyerButton,
+  LazyerSwitch,
+  LazyerTabsItem,
+  LazyerTabs,
+  LazyerDialog,
+  openDialog,
+  LazyerDropdown,
+  LazyerMenuItem
+} from 'lazyer-ui-1'
 import 'lazyer-ui-1/dist/lib/lazyer.css'
 
 export default {
   name: 'App',
   components: {
-    Button, Switch, Tabs, Dialog, openDialog, Tab
+    LazyerButton, LazyerSwitch, LazyerTabs, LazyerDialog, openDialog, LazyerTabsItem, LazyerDropdown, LazyerMenuItem
   },
   data() {
     return {
@@ -137,8 +161,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
